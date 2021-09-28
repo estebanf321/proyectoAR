27e805be2e66fa53e54a8649bcde038bf03849c6
Esteban Famoso <estebanf.it@gmail.com>
2021-09-28 20:21:09 -0300

    carga a hosting proyectoAR 3-001

--- procesaEmai.php
+++ procesaEmai.php
@@ -1,11 +1,66 @@
-<!DOCTYPE html>
+<?php
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$tel=$_POST['tel'];
+$feedBack=$_POST['feedBack'];
+
+ValidarDatos($name);
+ValidarDatos($email);
+ValidarDatos($tel);
+ValidarDatos($feedBack);
+
+
+$toAdreess='ventas@desarmaderoar.com.ar';
+$subject='Consultas desde la web';
+$mailContent='- Nombre : '.$name."<br/>"
+			.'- E-mail : '.$email."<br/>"
+			.'- Telefono : '.$tel."<br/>"
+			.'- Consulta : '.$feedBack."<br/>";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: $email";
+
+ini_set(sendmail_from,'ventas@desarmaderoar.com.ar');
+
+
+mail($toAdreess, $subject, $mailContent, $headers);  
+
+
+
+
+  function ValidarDatos($campo){
+    //Array con las posibles cabeceras a utilizar por un spammer
+    $badHeads = array("Content-Type:",
+                                 "MIME-Version:",
+                                 "Content-Transfer-Encoding:",
+                                 "Return-path:",
+                                 "Subject:",
+                                 "From:",
+                                 "Envelope-to:",
+                                 "To:",
+                                 "bcc:",
+                                 "cc:");
+
+    //Comprobamos que entre los datos no se encuentre alguna de
+    //las cadenas del array. Si se encuentra alguna cadena se
+    //dirige a una p�gina de Forbidden
+    foreach($badHeads as $valor){
+      if(strpos(strtolower($campo), strtolower($valor)) !== false){
+        header("HTTP/1.0 403 Forbidden");
+        exit;
+      }
+    }
+  }
+?>
+
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novedades - Autopartes Rosario</title>
+    <title>Consulta Enviada - Autopartes Rosario</title>
    <!--fuentes del sitio-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -41,15 +96,17 @@
                 <li> <a href="empresa.html"><i class="fas fa-building"></i>Empresa</a></li>
                 <li> <a href="servicios.html"><i class="fab fa-usps"></i>Servicios</a></li>
                 <li> <a href="productos.html"><i class="fas fa-car"></i>Productos</a></li>
-                <li> <a class="activo" href="novedades.html"><i class="fas fa-newspaper"></i>Novedades</a></li>
+                <li> <a href="novedades.html"><i class="fas fa-newspaper"></i>Novedades</a></li>
                 <li> <a href="contacto.html"><i class="fas fa-mail-bulk"></i>Contacto</a></li>
             </ul>
         </div>
     </nav>
 
     <main class="holder">
-        <h2>Novedades</h2>
-        <p>Sitio En Construcción.</p>
+        <h2>CONSULTA ENVIADA</h2>
+        <p>¡MUCHAS GRACIAS POR HABER REALIZADO SU CONSULTA!...</p>
+        <p > a la brevedad estaremos poniendonos en contacto con usted
+              para dar una respuesta a la misma</p>
 
 
         <div class="homeimg">
@@ -57,30 +114,6 @@
         </div>
 
         
-
-        <!-- <div class="novedades">
-            <h3>OFERTAS</h3>
-            <h4>Espejos Eléctricos</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero pariatur tempora nostrum repudiandae error
-                officia nam asperiores alias nesciunt, quos labore maiores corrupti, consequuntur cum minima
-                perspiciatis repellat? Quia, omnis!</p>
-        </div>
-
-        <div class="novedades">
-            <h3>CAPACITACION</h3>
-            <h4>Para Talleres zona: Litoral</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur blanditiis quis iure ullam
-                voluptatibus explicabo laudantium at nobis, et nulla? Tempore voluptas officiis facilis sunt assumenda
-                provident in excepturi distinctio.</p>
-        </div>
-
-        <div class="novedades">
-            <h3>INGRESOS</h3>
-            <h4>Chapería Linea Volkswagen</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolores voluptatum repudiandae odit, qui
-                quidem maiores recusandae a ipsum, veritatis quo. Animi in, cum quas architecto quos dolorum nihil.
-                Corrupti.</p>
-        </div> -->
 
 
     </main>
